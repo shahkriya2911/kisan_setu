1662bd479ff18482eaa05c0e0cc4201234c1e723
nirmalsivadas <nirmalsivadas2017@gmail.com>
2026-02-13 10:58:35 +0530

    User entity created

---
+++ src/main/java/com/project/kisan_setu/entity/User.java
@@ -0,0 +1,40 @@
+package com.project.kisan_setu.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity // table creation
+@Table(name = "users") //table name
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userFullName;
+
+    @Column(nullable = false,unique = true)
+    private String userEmail;
+
+    @Column(nullable = false,unique = true)
+    private String userPhoneNumber;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(nullable = false)
+    private String userCreatedAt;
+
+    @Column(nullable = false)
+    private String userAddress;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private UserRole userRole;
+}
