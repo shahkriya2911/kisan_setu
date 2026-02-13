cb8407f44f082d714c521054d82300e3f2410662
nirmalsivadas <nirmalsivadas2017@gmail.com>
2026-02-13 11:27:07 +0530

    Connected user with roles

--- src/main/java/com/project/kisan_setu/entity/User.java
+++ src/main/java/com/project/kisan_setu/entity/User.java
@@ -35,6 +35,6 @@ public class User {
     @Column(nullable = false)
     private String userAddress;
 
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    private UserRole userRole;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+    private List<UserRole> userRole;
 }
