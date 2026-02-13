472b48d98d76f9879ee0e8d8739b614b2b6c204e
Kriya Shah <kriyashah2911@gmail.com>
2026-02-13 09:55:02 -0500

    Intial Commit

---
+++ src/main/java/com/project/kisan_setu/KisanSetuApplication.java
@@ -0,0 +1,13 @@
+package com.project.kisan_setu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisanSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisanSetuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kisan_setu/KisanSetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kisan_setu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisanSetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
