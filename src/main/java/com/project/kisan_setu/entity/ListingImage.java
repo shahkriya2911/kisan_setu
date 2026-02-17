c1d3c9266f6862ad5de480fcccbd5922edb265f3
Kriya Shah <kriyashah2911@gmail.com>
2026-02-17 15:54:20 -0500

    change in certificate and listing image entity

--- src/main/java/com/project/kisan_setu/entity/ListingImage.java
+++ src/main/java/com/project/kisan_setu/entity/ListingImage.java
@@ -1,17 +1,27 @@
 package com.project.kisan_setu.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Table(name = "listing_images")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ListingImage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long imageId;
 
-    private String imageUrl;
-
+    private String fileName;
+    private String filePath;
+    private String fileType;
     private Boolean isPrimary; // for cover image
 
     @ManyToOne
--- src/main/java/com/project/kisan_setu/entity/QualityCertificate.java
+++ src/main/java/com/project/kisan_setu/entity/QualityCertificate.java
@@ -1,20 +1,29 @@
 package com.project.kisan_setu.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "quality_certificates")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class QualityCertificate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long certificateId;
-    private MultipartFile qualityCertificate;
     private String certificateName;
     private LocalDate issuedDate;
+    private String fileName;
+    private String fileType;
 
     @OneToOne
     @JoinColumn(name = "listing_id")
