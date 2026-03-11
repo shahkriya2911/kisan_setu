84ed0aa14ec0f4fc267df6783e20c85729428f7e
nirmalsivadas <nirmalsivadas2017@gmail.com>
2026-03-11 10:27:50 +0530

    Terms and Policies

---
+++ src/main/java/com/project/kisan_setu/service/impl/TermsAndPoliciesImpl.java
@@ -0,0 +1,25 @@
+package com.project.kisan_setu.service.impl;
+
+import com.project.kisan_setu.dto.TermsAndPoliciesResponseDto;
+import com.project.kisan_setu.entity.TermsAndPolicies;
+import com.project.kisan_setu.mapper.TermsAndPoliciesMapper;
+import com.project.kisan_setu.repository.TermsAndPoliciesRepository;
+import com.project.kisan_setu.service.TermsAndPoliciesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TermsAndPoliciesImpl implements TermsAndPoliciesService {
+    private final TermsAndPoliciesRepository termsAndPoliciesRepository;
+
+    public TermsAndPoliciesImpl(TermsAndPoliciesRepository termsAndPoliciesRepository) {
+        this.termsAndPoliciesRepository = termsAndPoliciesRepository;
+    }
+
+    @Override
+    public TermsAndPoliciesResponseDto getTermsAndPolicies(){
+        TermsAndPolicies policies =  termsAndPoliciesRepository.findTopByOrderByTermsAndPoliciesIdAsc()
+                .orElseThrow(() -> new RuntimeException("Policies not found"));
+        return TermsAndPoliciesMapper.toDto(policies);
+
+    }
+}
