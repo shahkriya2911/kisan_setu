3780f795c26b01167db326a6f5d20174cc8031aa
nirmalsivadas <nirmalsivadas2017@gmail.com>
2026-03-20 09:39:17 +0530

    Conflict resolve

--- src/main/java/com/project/kisan_setu/service/impl/OrderServiceImpl.java
+++ src/main/java/com/project/kisan_setu/service/impl/OrderServiceImpl.java
@@ -25,7 +25,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
