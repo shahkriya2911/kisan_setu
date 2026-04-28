f2559d06586980ca4f0d40e19c115b105c5e4f9a
nirmalsivadas <nirmalsivadas2017@gmail.com>
2026-04-28 12:18:11 +0530

    Specific file change 2

--- src/main/java/com/project/kisan_setu/security/CorsConfigurationSourceConfig.java
+++ src/main/java/com/project/kisan_setu/security/CorsConfigurationSourceConfig.java
@@ -18,7 +18,7 @@ public class CorsConfigurationSourceConfig {
 
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:3000",
-                "https://kissansetu.vercel.app/"
+                "https://kissansetu.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
