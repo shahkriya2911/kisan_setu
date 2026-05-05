eec79ee210f14a9fbbc528c9d80bb81a2573de13
Kriya Shah <kriyashah2911@gmail.com>
2026-05-05 12:17:53 +0530

    set cookie in header in security config

--- src/main/java/com/project/kisan_setu/security/CorsConfig.java
+++ src/main/java/com/project/kisan_setu/security/CorsConfig.java
@@ -1,16 +1,13 @@
 package com.project.kisan_setu.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
